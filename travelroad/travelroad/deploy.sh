6de334d2cc871056956c77d2056a8f4ee04ae41e
Flouisss <luisfloresafl@gmail.com>
2024-01-22 16:31:10 +0000

    Se Realizan cambios en las maquina de desarrollo

---
+++ travelroad/travelroad/deploy.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+sudo cp -r /usr/share/nginx/travelroad /home/pc12-dpl/travelroad_laravel/travelroad/
+
+cd /home/pc12-dpl/travelroad_laravel/
+
+git add .
+git commit -m "Se Realizan cambios en las maquina de desarrollo"
+git push
+
+ssh Luis@20.56.155.34 "
+	cd /home/Luis/travelroad_laravel/travelroad
+	git fetch
+	git pull
+
+	cd travelroad_laravel/
+	sudo cp -r travelroad/ /usr/share/nginx/
+	cd /usr/share/nginx/travelroad
+	sudo composer install
+	sudo systemctl restart nginx
+"
