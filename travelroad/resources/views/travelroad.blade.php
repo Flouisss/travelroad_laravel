4fda3123deb9f12c54d2563cea91d73d8780077a
Flouisss <luisfloresafl@gmail.com>
2024-02-19 16:58:41 +0000

    Se Realizan cambios en las maquina de desarrollo

--- travelroad/resources/views/travelroad.blade.php
+++ travelroad/resources/views/travelroad.blade.php
@@ -6,8 +6,8 @@
   <body>
     <h1>My Travel Bucket List</h1>
     <ul>
-	<li><a href="{{ url()/wished}}">Places I'd like to visit</a></li>
-	<li><a href="{{ url()/visited}}">Places I've Already Been To</a></li>
+	<li><a href="{{ url('/wished')}}">Places I'd like to visit</a></li>
+	<li><a href="{{ url('/visited')}}">Places I've Already Been To</a></li>
     <ul>
     <li><p>Powered by <strong>Laravel</strong></p></li>
   </body>
